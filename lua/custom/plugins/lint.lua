fd55a151b5e4657a56d685747f8d59c7dbdf0185
Kyle Kartchner <kkartch@gmail.com>
2024-10-26 11:41:36 -0600

    Separated additional plugins to individual files, fixed conform formatting, and added linting

---
+++ lua/custom/plugins/lint.lua
@@ -0,0 +1,21 @@
+return {
+  'mfussenegger/nvim-lint',
+  config = function()
+    require('lint').linters_by_ft = {
+      lua = { 'luacheck' },
+      python = { 'flake8' },
+      javascript = { 'eslint' },
+      typescript = { 'eslint' },
+      go = { 'golangci-lint' },
+      ruby = { 'rubocop' },
+      elixir = { 'credo' },
+      -- Add more filetypes and their respective linters here
+    }
+
+    vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+      callback = function()
+        require('lint').try_lint 'credo'
+      end,
+    })
+  end,
+}
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,3 +4,6 @@ vim.keymap.set('n', '<leader>gg', ':LazyGit<CR>')
 -- Window resizing
 vim.keymap.set('n', '<CR>', '<c-w>_<c-w>|')
 vim.keymap.set('n', '<c-i>', '<c-w>=')
+
+-- Linting
+vim.keymap.set('n', '<leader>l', '<CMD>lua require("lint").try_lint("credo")<CR>')
